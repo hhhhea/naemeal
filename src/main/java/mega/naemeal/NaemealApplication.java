9d17f42706403ecdac2992d2f97004c071e11e3a
EUIJOON <euijoonism@gmail.com>
2023-07-05 01:04:13 +0900

    First Commit

---
+++ src/main/java/mega/naemeal/NaemealApplication.java
@@ -0,0 +1,13 @@
+package mega.naemeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaemealApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NaemealApplication.class, args);
+  }
+
+}
---
+++ src/test/java/mega/naemeal/NaemealApplicationTests.java
@@ -0,0 +1,13 @@
+package mega.naemeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaemealApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
