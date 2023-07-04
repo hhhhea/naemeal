efee209588c9c52afd7a67f4184bea4e85305c4e
EUIJOON <euijoonism@gmail.com>
2023-07-05 08:20:18 +0900

    [feat]#5.Service단, Repository 개발

---
+++ src/main/java/mega/naemeal/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package mega.naemeal.user.repository;
+
+import java.util.Optional;
+import mega.naemeal.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+  Optional<User> findByUserId(String userId);
+  Optional<Object> findByUserId(UserDetailsImpl userDetails);
+}
---
+++ src/main/java/mega/naemeal/user/service/UserServiceImpl.java
@@ -0,0 +1,77 @@
+package mega.naemeal.user.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import mega.naemeal.enums.UserRoleEnum;
+import mega.naemeal.user.entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserServiceImpl {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  private static final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+  private final ProfileRepository profileRepository;
+  private static String basicProfileImage = "profile/profile-basic.jpg";
+  public static final String CLOUD_FRONT_DOMAIN_NAME = "https://d261u93iebql1x.cloudfront.net/";
+
+  @Override
+  public void signup(SignupRequestDto requestDto) {
+    String userId = requestDto.getUserId();
+    String password = passwordEncoder.encode(requestDto.getPassword());
+    String nickname = requestDto.getNickname();
+
+    Optional<User> found = userRepository.findByUserId(requestDto.getUserId());
+    if (found.isPresent()) {
+      throw new IllegalArgumentException("중복된 아이디가 존재합니다.");
+    }
+    UserRoleEnum role = UserRoleEnum.USER;
+
+    if (requestDto.getAdminToken() != null) {
+      if (!requestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+        throw new IllegalArgumentException("관리자 암호가 일치하지 않아 등록할 수 없습니다.");
+      }
+      role = UserRoleEnum.ADMIN;
+    }
+
+    User user = new User(userId, password, nickname, role);
+    Profile profile = new Profile(userId, nickname, CLOUD_FRONT_DOMAIN_NAME+basicProfileImage);
+    userRepository.save(user);
+    profileRepository.save(profile);
+  }
+
+  @Override
+  public AuthenticatedUserInfoDto signin(SigninRequestDto requestDto) {
+    User user = userRepository.findByUserId(requestDto.getUserId()).orElseThrow(
+        () -> new IllegalArgumentException("등록된 아이디가 없습니다.")
+    );
+    if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
+      throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+    }
+    return new AuthenticatedUserInfoDto(user.getRole(), user.getUserId());
+  }
+
+  @Override
+  public void signout(HttpServletRequest request) {
+  }
+
+  @Override
+  public void unregister(String userId, PasswordcheckRequestDto requestDto) {
+    User user = userRepository.findByUserId(userId).orElseThrow(
+        () -> new IllegalArgumentException("등록된 아이디가 없습니다.")
+    );
+    if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
+      throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+    }
+    user.changeRole(UserRoleEnum.UNREGISTER);
+    userRepository.save(user);
+  }
+
+}
