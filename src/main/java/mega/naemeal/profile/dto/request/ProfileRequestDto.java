1281d233ee06f710a9962c4b8308e80b2b5d2691
EUIJOON <euijoonism@gmail.com>
2023-07-08 01:21:29 +0900

    [feat] Profile - Dto 계층 생성

---
+++ src/main/java/mega/naemeal/profile/dto/request/ProfileRequestDto.java
@@ -0,0 +1,12 @@
+package mega.naemeal.profile.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class ProfileRequestDto {
+
+  private String nickname;
+  private String tel;
+  private String password;
+
+}
